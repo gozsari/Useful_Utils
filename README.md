3f3837d0fd27e1230b41ed0bfdf3d33cfc557854
gozsari <gozsari@gmail.com>
2023-03-22 17:31:39 +0300

    update

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 [![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
 [![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
 
-Some tools facilitate our work
+## Tools facilitate our work
+### 1- Extracting the table from the PDF files:
+- You can extract the table from the PDF files using the `extract_pdf_tables.py` file.
+- Input files should be located in the **input** directory.
+- Usage: `python extract_pdf_tables.py --pdf_file <input_file> --page_no <page_number_in_pdf> --table_no <table_number_in_page>`
+- Example: `python extract_pdf_tables.py --pdf_file "test.pdf" --page_no 1 --table_no 1`
+- Output: `table1.csv` and `table1.xlsx` files will be created in the **output** directory.
 
 
--- extract_pdf_tables.py
+++ extract_pdf_tables.py
@@ -1,5 +1,5 @@
 """
-This script extracts tables from a pdf file and saves them as csv and xlsx files.
+This script extracts output from a pdf file and saves them as csv and xlsx input.
 """
 import tabula
 import argparse
@@ -7,16 +7,16 @@ import argparse
 def main():
     """Main function"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pdf_file', type=str, default='files/dataset_add_info.pdf', help='pdf file name')
+    parser.add_argument('--pdf_file', type=str, default='input/test.pdf', help='pdf file name')
     parser.add_argument('--page_no', type=int, default=6, help='page number')
     parser.add_argument('--table_no', type=int, default=2, help='table number')
     args = parser.parse_args()
     pdf_file = args.pdf_file
     page_no = args.page_no
     table_no = args.table_no
-    dfs = tabula.read_pdf(f"files/{pdf_file}", pages=page_no)
-    dfs[table_no].to_csv(f"tables/table_{table_no}.csv", index=False)
-    dfs[table_no].to_excel(f"tables/table_{table_no}.xlsx", index=False)
+    dfs = tabula.read_pdf(f"input/{pdf_file}", pages=page_no)
+    dfs[table_no].to_csv(f"output/table_{table_no}.csv", index=False)
+    dfs[table_no].to_excel(f"output/table_{table_no}.xlsx", index=False)
 
 
 if '__main__' == __name__:
