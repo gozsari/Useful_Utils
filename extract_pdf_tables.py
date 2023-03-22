2bd8050b1bd56ccc85ffbde7fb5d888878a0d2e6
gozsari <gozsari@gmail.com>
2023-03-22 16:05:40 +0300

    extract tables from pdf

--- extract_pdf_tables.py
+++ extract_pdf_tables.py
@@ -1,9 +1,23 @@
+"""
+This script extracts tables from a pdf file and saves them as csv and xlsx files.
+"""
 import tabula
-import pandas as pd
-pdf_file = "files/dataset_add_info.pdf"
-page_no = 6
-table_no = 2
-dfs = tabula.read_pdf(pdf_file, pages=page_no)
+import argparse
 
-dfs[table_no].to_csv(f"tables_csv/table_{table_no}.csv", index=False)
-dfs[table_no].to_excel(f"tables_csv/table_{table_no}.xlsx", index=False)
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pdf_file', type=str, default='files/dataset_add_info.pdf', help='pdf file name')
+    parser.add_argument('--page_no', type=int, default=6, help='page number')
+    parser.add_argument('--table_no', type=int, default=2, help='table number')
+    args = parser.parse_args()
+    pdf_file = args.pdf_file
+    page_no = args.page_no
+    table_no = args.table_no
+    dfs = tabula.read_pdf(f"files/{pdf_file}", pages=page_no)
+    dfs[table_no].to_csv(f"tables/table_{table_no}.csv", index=False)
+    dfs[table_no].to_excel(f"tables/table_{table_no}.xlsx", index=False)
+
+
+if '__main__' == __name__:
+    main()
